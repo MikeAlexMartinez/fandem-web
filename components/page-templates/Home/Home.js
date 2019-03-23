bbfa05f16c5e0dd8ea7e98a3bc14cda7ca7690de
Michael Alex Martinez <mike.alex.martinez@gmail.com>
2019-03-23 18:49:14 +0000

    added home page and background

--- components/page-templates/Home/Home.js
+++ components/page-templates/Home/Home.js
@@ -1,8 +1,47 @@
+import React, { Component } from "react";
+import withStyles from "@material-ui/core/styles/withStyles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { Typography } from "@material-ui/core";
+import Background from "../../shared/Background/Background";
 
-const Home = props => (
-  <div className="title">
-    This is Home!
-  </div>
-);
+const backgroundShape = "static/images/shape.svg";
 
-export default Home;
+const styles = theme => {
+  console.log(theme);
+  return {
+    root: {
+      flexGrow: 1
+    },
+    header: {
+      marginTop: "100px",
+      marginBottom: "100px"
+    },
+    leftmargin: {
+      marginLeft: "100px"
+    }
+  };
+};
+
+class Home extends Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <Background>
+        <CssBaseline />
+        <div className={classes.root}>
+          <div className={`${classes.header} ${classes.leftmargin}`}>
+            <Typography variant="h1">fandem.io</Typography>
+          </div>
+          <div className={`${classes.subheader} ${classes.leftmargin}`}>
+            <Typography variant="headline">
+              Where football fans come to demonstrate how accurate their
+              predictions are.
+            </Typography>
+          </div>
+        </div>
+      </Background>
+    );
+  }
+}
+
+export default withStyles(styles)(Home);
