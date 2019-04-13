73c68b1eaa1613c9b0c223c981958f01e9e9ecd5
Michael Alexander Martinez <mike.alex.martinez@gmail.com>
2019-04-13 11:55:58 +0100

    added dashboard and navigation component and fixed apollo-boost

--- components/page-templates/Dashboard/Dashboard.js
+++ components/page-templates/Dashboard/Dashboard.js
@@ -1,7 +1,11 @@
+import { withStyles } from "@material-ui/core";
+import Navigation from "../../shared/Navigation/Navigation";
 
+import styles from "./Dashboard.styles";
 
 const Dashboard = props => (
   <div>
+    <Navigation />
     This is The Dashboard!
   </div>
 );
